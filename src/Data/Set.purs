3689ae512089228ec040135881f65e2f3ef9f1b7
Emil Haugberg <emilveen@gmail.com>
2017-01-30 00:13:08 +0100

    Add symbols and code cleanup.

--- src/Data/Set.purs
+++ src/Data/Set.purs
@@ -7,29 +7,37 @@ import Data.Monoid (class Monoid)
 import Prelude
 
 type Collection = Array
-data Set a = Set (Array a)
+newtype Set a = Set (Array a)
+
+infix 8 contains as ∈
 
 -- | check if a set contains a value a
 contains :: forall a. Eq a => a -> Set a -> Boolean
 contains val = maybe false (const true) <<< Arr.findIndex (eq val) <<< fromSet
 
+infix 8 subset as ⊆
+
 -- | check if set a is a subset of b
 subset :: forall a. Eq a => Set a -> Set a -> Boolean
 subset a b =
-  foldl (\bool a' -> bool && contains a' b) true a
+  foldl (\bool a' -> bool && a' ∈ b) true a
 
 -- | check if set a is a proper subset of b
 proper :: forall a. (Eq a, Ord a) => Set a -> Set a -> Boolean
-proper a b = subset a b && not (eq a b)
+proper a b = a ⊆ b && not (eq a b)
 
 -- | check if sets are transitive
 -- | transitive sets A, B, and C such that if A `f` B and B `f` C then A `f` C
 transitive :: forall a. (Set a -> Set a -> Boolean) -> Set a -> Set a -> Set a -> Boolean
-transitive f a b c = if a `f` b && b `f` c then true else false
+transitive f a b c = a `f` b && b `f` c
+
+infix 8 union as ∪
 
 -- | Union of a collection of sets is the set of all elements in the collection
 union :: forall a. Collection (Set a) -> Set a
 union = foldl append empty
+
+infix 8 intersection as ∩
 
 -- | the intersection A ∩ B of two sets A and B is the set that contains
 -- | all elements of A that also belong to B (or the other way around).
--- test/Main.purs
+++ test/Main.purs
@@ -1,17 +1,17 @@
 module Test.Main where
 
-import Prelude
+import Prelude (Unit, bind, eq, ($))
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE,log)
-import Data.Set
+import Control.Monad.Eff.Console (CONSOLE)
+import Data.Set (Set(..), disjoint, intersection, proper, union, (∈), (⊆))
 import Data.Set (empty) as Set
-import Test.Unit (suite,test,timeout)
-import Test.Unit.Main (runTest)
+import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
-import Test.Unit.Console
+import Test.Unit.Main (runTest)
+import Test.Unit.Console (TESTOUTPUT)
 
-main :: forall a eff. Eff ("console" :: CONSOLE, "testOutput" :: TESTOUTPUT, "avar" :: AVAR | eff) Unit
+main :: forall eff. Eff ("console" :: CONSOLE, "testOutput" :: TESTOUTPUT, "avar" :: AVAR | eff) Unit
 main = runTest do
   suite "Basic set functions" do
     test "equals" do
@@ -20,13 +20,13 @@ main = runTest do
       Assert.assertFalse "Should not be equal " $ eq (Set [5,4])    (Set [3,5,4])
 
     test "contains" do
-      Assert.assert      "Should contain 5"     $ contains 5 (Set [1,2,4,5])
-      Assert.assertFalse "Should not contain 5" $ contains 5 (Set [1,2,4])
+      Assert.assert      "Should contain 5"     $ 5 ∈ Set [1,2,4,5]
+      Assert.assertFalse "Should not contain 5" $ 5 ∈ Set [1,2,4]
 
     test "subset" do
-      Assert.assert      "Should be a subset"            $ subset (Set [1,2,3]) (Set [1,3,4,2,5])
-      Assert.assert      "Equal sets should be a subset" $ subset (Set [1,2,3]) (Set [3,2,1])
-      Assert.assertFalse "Should not be a subset"        $ subset (Set [3,4])   (Set [1,2,4])
+      Assert.assert      "Should be a subset"            $ Set [1,2,3] ⊆ Set [1,3,4,2,5]
+      Assert.assert      "Equal sets should be a subset" $ Set [1,2,3] ⊆ Set [3,2,1]
+      Assert.assertFalse "Should not be a subset"        $ Set [3,4]   ⊆ Set [1,2,4]
 
     test "proper" do
       Assert.assert      "Should be a subset"                   $ proper (Set [1,2,3]) (Set [1,2,3,4,5])
